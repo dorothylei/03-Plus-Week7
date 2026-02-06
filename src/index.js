c4e171d825d9bdc5a5b7c9c4c56a115eb4401a35
Dorothy <say.hello.dot.design@gmail.com>
2026-02-06 12:26:50 +0800

    Add search functionality to JavaScript
    using API data.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  /* console.log(response.data.temperature.current); */
+}
+
+function searchCity(city) {
+  /* This is to search the weather information according to the city value of the user's input */
+  let apiKey = "eeb99atb8c0co74c0e6854d22838404f";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  /* console.log(apiUrl) */
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
+  /* This is to send the search input value to the searchCity function. */
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Barcelona");
