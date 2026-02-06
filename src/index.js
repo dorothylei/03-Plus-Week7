83d0975682ea3201f0c5516823e16c6814516783
Dorothy <say.hello.dot.design@gmail.com>
2026-02-06 15:23:21 +0800

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,44 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#weather-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  /*console.log(response.data);
+  console.log(response.data.condition.description);*/
 
   cityElement.innerHTML = response.data.city;
+
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 
   /* console.log(response.data.temperature.current); */
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+if (minutes < 10) {
+  minutues = `0${minutes}`;
 }
 
 function searchCity(city) {
